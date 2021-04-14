4ebc2b909aaca74ea029735805bd5ca0b0b1909d
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-14 23:35:41 +0900

    Sass使用可能に

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@
 export default {}
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -42,16 +42,7 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Josefin Slab', serif;
   display: block;
   font-weight: 300;
   font-size: 100px;

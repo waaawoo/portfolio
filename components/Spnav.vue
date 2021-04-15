3c51a92ac7b1bc13e89c91fee879ccabc11bd870
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-15 11:00:07 +0900

    ヘッダー部SP版

---
+++ components/Spnav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>SP</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

affb43355b1223c4755afefca4253dac256a2665
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-17 17:49:10 +0900

    タイトル修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'MK portfolio',
     htmlAttrs: {
       lang: 'en'
     },

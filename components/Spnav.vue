fe446060937f46a5eac8e901dcd4c8a45c1aedbf
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-17 17:43:55 +0900

    バーガーメニュー修正

--- components/Spnav.vue
+++ components/Spnav.vue
@@ -1,73 +1,66 @@
 <template>
   <div class="sp-header">
-    <i class="fas fa-times" v-if="$store.state.spClick" @click="$store.commit('changFlg')"></i>
+    <i
+      class="fas fa-times"
+      v-if="$store.state.spClick"
+      @click="$store.commit('changFlg')"
+    ></i>
     <ul class="sp-nav" @click="$store.commit('changFlg')">
-      <li><nuxt-link active-class="link-active" to="/" exact>Home</nuxt-link></li>
-      <li><nuxt-link active-class="link-active" to="/works">Works</nuxt-link></li>
-      <li class="last-li"><nuxt-link active-class="link-active" to="/contact">Contact</nuxt-link></li>
+      <nuxt-link active-class="link-active" to="/" exact
+        ><li>Home</li></nuxt-link
+      >
+      <nuxt-link active-class="link-active" to="/works"
+        ><li>Works</li></nuxt-link
+      >
+      <nuxt-link active-class="link-active" to="/contact"
+        ><li>Contact</li></nuxt-link
+      >
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-
   data() {
-    return {
-
-    };
+    return {};
   },
-  mounted() {
-
-  },
-  methods: {
-
-  },
+  mounted() {},
+  methods: {},
 };
 </script>
 
 <style lang="scss">
-  .sp-header{
-    display: inline;
-    position: fixed;
-    top: 0;
-    width: 95%;
-    z-index: 98;
+.sp-header {
+  display: inline;
+  position: fixed;
+  top: 0;
+  width: 95%;
+  z-index: 98;
 
-    .fa-times{
-      top: 0px;
-      display: block;
-      text-align: right;
-      margin: 30px 50px 10px 0;
-      font-size: 2em;
-    }
-    .sp-nav{
-      text-align: center;
-      padding: 0;
-      li{
+  .fa-times {
+    top: 0px;
+    display: block;
+    text-align: right;
+    margin: 30px 50px 10px 0;
+    font-size: 2em;
+  }
+  .sp-nav {
+    text-align: center;
+    padding: 100px 0 300px 0;
+    background: rgba($color: #333, $alpha: 0.6);
+    a {
+      text-decoration: none;
+      color: #fff;
+      li {
         transition: all 0.5s;
         list-style: none;
         padding: 15px;
+        margin-top: 10%;
         border: 1px solid #fff;
-        border-bottom: none;
         font-weight: bold;
         background: #333;
-        &:hover{
-          transition: all 0.5s;
-          background: #fff;
-          border: 1px solid #333;
-          a{
-            color: #333;
-          }
         }
-        a{
-          text-decoration: none;
-          color: #fff;
-        }
-      }
-      .last-li{
-        border-bottom: 1px solid #fff;
-      }
     }
   }
+}
 </style>

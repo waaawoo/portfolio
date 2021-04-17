67952e9f07bea2f5a4988612b29f226b861cf17e
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-17 18:21:28 +0900

    ローカルポート番号修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,8 +48,8 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
-  server: {
-    port: 3333,
-    host: "0.0.0.0"
-  }
+  // server: {
+  //   port: 3333,
+  //   host: "0.0.0.0"
+  // }
 }
